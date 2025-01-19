534517db817f30943bfb85b2c4dc93dff2ee47db
Mykyta Batrak <mykbatrak@gmail.com>
2025-01-19 03:57:57 -0600

    Added mini.bracketed plugin

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -2,6 +2,22 @@ local function config()
   require("mini.statusline").setup({ use_icons = true })
   require("mini.ai").setup({ n_lines = 500 })
   require("mini.surround").setup()
+  require("mini.bracketed").setup({
+    buffer = { suffix = "b", options = {} },
+    comment = { suffix = "" },
+    conflict = { suffix = "" },
+    diagnostic = { suffix = "" },
+    file = { suffix = "" },
+    indent = { suffix = "" },
+    jump = { suffix = "" },
+    location = { suffix = "" },
+    oldfile = { suffix = "" },
+    quickfix = { suffix = "" },
+    treesitter = { suffix = "" },
+    undo = { suffix = "" },
+    window = { suffix = "" },
+    yank = { suffix = "" },
+  })
 end
 
 return {
