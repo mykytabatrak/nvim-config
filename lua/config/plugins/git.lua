a1ef0dd89e79fcab73e83fd985b53d50d47b8e00
Mykyta Batrak <mykbatrak@gmail.com>
2026-02-03 13:13:51 -0600

    Added Neogit

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -7,4 +7,13 @@ return {
     "lewis6991/gitsigns.nvim",
     config = config,
   },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+    },
+    cmd = "Neogit",
+  },
 }
