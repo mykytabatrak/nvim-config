cf2c7430ed6357edbe50a96e6a8118812ee001bb
Mykyta Batrak <mykbatrak@gmail.com>
2026-02-09 08:19:21 -0600

    Add diffview as plugin

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -8,11 +8,13 @@ return {
     config = config,
   },
   {
+    "sindrets/diffview.nvim",
+  },
+  {
     "NeogitOrg/neogit",
     lazy = true,
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
     },
     cmd = "Neogit",
   },
